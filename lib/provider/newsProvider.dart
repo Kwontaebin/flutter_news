bc1ef64b90963764aff381562f197844cc32b8c0
kwontaebin <144872939+Kwontaebin@users.noreply.github.com>
2025-09-25 14:23:37 +0900

    상태코드에 따른 예외처리 추가

--- lib/provider/newsProvider.dart
+++ lib/provider/newsProvider.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../component/api/dio.dart';
@@ -53,9 +54,17 @@ class NewsProvider extends ChangeNotifier {
       if (newsList.length >= totalResults || newsList.length >= 100) {
         hasMore = false;
       }
+    } on DioException catch (e) {
+      final statusCode = e.response?.statusCode;
+
+      if (statusCode == 400) return showToast(message: "키워드 입력이 잘못되었습니다");
+      if (statusCode == 429) return showToast(message: "요청 횟수를 초과했습니다. 잠시후 다시 시도해주세요");
+
+      return;
     } catch (e) {
-      print("공습경보: $e");
-      showToast(message: "에러가 발생했습니다.\n잠시 후 다시 시도해 주세요");
+      print("기타 에러: $e");
+
+      return;
     } finally {
       isLoading = false;
       notifyListeners();
